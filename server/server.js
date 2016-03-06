ebe3df8f11b96861f21f55ebacc81f3c55ca0db9
wellslogan <wellslogan@gmail.com>
2016-03-05 19:23:31 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,82 @@
+/**
+ * Created by Logan on 3/5/16.
+ */
+var _ = require('underscore');
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/plex', function(req, res) {
+
+
+    //res.send(_.filter([1, 2, 3], function(item) {return item % 2 == 0;}));
+});
+
+app.post('/plex', function(req, res) {
+    var body = req.body;
+
+    processMessage(req.body.text, req.body.name);
+
+});
+
+processMessage = function(msg, name) {
+    var keyword = "@plex";
+    if (msg.length > keyword.length && msg.substring(0, keyword.length + 1) == keyword) {
+        var result = callPlexApi(msg.substring(keyword.length));
+
+        postToGroup(result);
+    }
+}
+
+postToGroup = function(msg, botId) {
+    var options = {
+        url: "https://api.groupme.com/v3/bots/post",
+        method: "POST",
+        json: true,
+        body: {
+            "bot_id": botId,
+            "text": msg
+        }
+    };
+
+    request(options, function(error, response) {
+        if (error || response.statusCode != 200) {
+            console.log(error);
+        }
+    });
+};
+
+callPlexApi = function(query) {
+    var options = {
+        url: "/search?query=" + query,
+        headers: {
+            "Accept": "application/json",
+            "X-Plex-Token": ""
+        },
+        json: true
+    };
+
+    request(options, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            return processPlexResults(body);
+        }
+    });
+};
+
+processPlexResults = function(data) {
+    var count = data["_children"].length;
+    if (count > 0) {
+        var firstTitle = data["_children"][0]["title"];
+        var firstYear = data["_children"][0]["year"];
+        return count + ' results found. Top result was "'+ firstTitle + '" (' + firstYear + ')';
+    } else {
+        return "No results found."
+    }
+}
+
+app.listen(3000);
